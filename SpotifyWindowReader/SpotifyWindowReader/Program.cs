482d604d920320be801262e8d22da44b4c781d76
omgmajk <majkieboy@gmail.com>
2021-10-24 19:56:51 +0200

    Updated code so it doesn't hammer the disk.

--- SpotifyWindowReader/SpotifyWindowReader/Program.cs
+++ SpotifyWindowReader/SpotifyWindowReader/Program.cs
@@ -18,7 +18,6 @@ namespace SpotifyWindowReader
             {
                 return "Spotify is not running!";
             }
-            // Change "Spotify Premium" to "Spotify" if you are not using premium version.
             if (string.Equals(proc.MainWindowTitle, "Spotify Premium", StringComparison.InvariantCultureIgnoreCase) ||
                 string.Equals(proc.MainWindowTitle, "Spotify", StringComparison.InvariantCultureIgnoreCase))
             {
@@ -38,9 +37,9 @@ namespace SpotifyWindowReader
         static void Main(string[] args)
         {
             SpotifyReader windowTitle = new SpotifyReader();
-            string songTitle;
+            string songTitle = string.Empty;
+            string tempSong;
             string path = @".\Song.txt";
-            bool x = true;
 
             Console.Title = "Spotify Song Reader";
             Console.OutputEncoding = Encoding.UTF8; // Have to set this manually or some songs will register weirdly.
@@ -48,18 +47,30 @@ namespace SpotifyWindowReader
             Console.WriteLine("Song Reader is active and printing to file: Song.txt.");
             Console.WriteLine("Turn off the application by using Ctrl+C or close the window.");
 
-            while (x == true)
+            while (true)
             {
-                songTitle = windowTitle.GetSpotifyTrackInfo();
+                Thread.Sleep(800); // 0.8 seconds for now.
+
+                tempSong = windowTitle.GetSpotifyTrackInfo();
+                if(tempSong != songTitle)
+                {
+                    songTitle = windowTitle.GetSpotifyTrackInfo();
+                }
+                else
+                {
+                    continue;
+                }
+                
                 //songTitle = songTitle.Replace("•", string.Empty); // Looks for the system bell character "•"
 
-                Thread.Sleep(800); // 0.8 seconds for now.
+               
 
                 // Write spotify title to file so it can be picked up by OBS
                 try
                 {
                     using (StreamWriter sw = File.CreateText(path))
                     {
+                        Console.WriteLine($"Writing {songTitle} to textfile");
                         sw.WriteLine(songTitle);
                     }
                 }
@@ -68,7 +79,7 @@ namespace SpotifyWindowReader
                     Console.WriteLine("\n\nFile not written, something went wrong...");
                     Console.WriteLine("\n\nPress any key to close the application.");
 
-                    x = false;
+                    break;
 
                     Console.ReadKey();
                 }
